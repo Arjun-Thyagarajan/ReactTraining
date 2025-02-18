920f98e3dbb4e9b898b99310c4e8a1da882fba34
Arjun-Thyagarajan <arjun.thyagrajan@solitontech.com>
2025-02-18 21:44:59 +0530

    setup: scss css
    Signed-off-by: Arjun-Thyagarajan <arjun.thyagrajan@solitontech.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName: "[name]_[local]_[hash:base64:5]",
+    },
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
 });
