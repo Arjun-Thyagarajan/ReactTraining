0d00b584970520d38bf3c226696a14c153c83d5b
Arjun-Thyagarajan <arjun.thyagarajan@solitontech.com>
2025-02-19 14:23:18 +0530

    chore: vitest setup
    Signed-off-by: Arjun-Thyagarajan <arjun.thyagarajan@solitontech.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -12,6 +13,12 @@ export default defineConfig({
       scss: {
         api: "modern-compiler",
       },
+    },
+  },
+  test: {
+    coverage: {
+      enabled: true,
+      reporter: ["text", "json", "html"],
     },
   },
 });
